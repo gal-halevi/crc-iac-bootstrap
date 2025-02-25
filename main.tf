68c651170d658edc62d9416c036a60c85574487d
Gal Halevi <93094499+gal-halevi@users.noreply.github.com>
2025-02-25 23:00:56 +0200

    Add bootstrap code

---
+++ main.tf
@@ -0,0 +1,43 @@
+# Create an S3 bucket for Terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  acl = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# Block public access for the S3 bucket
+resource "aws_s3_bucket_public_access_block" "tf_state_public_block" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Create a DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
